41955b82ccf43993845f909cef0fe17dd1428b8b
Kulyash123 <kulyash.orazbekova@ce.sdu.edu.kz>
2015-02-12 12:16:35 +0600

    Added my name to contributors.

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ def index():
     name = 'APT Spring 2015'
     participants = [
        'German Ilyin',
+        'Kulyash Orazbekova',
     ]
     return template("""
     <h1>HW2 for {{course}}</h1>
